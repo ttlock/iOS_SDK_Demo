92452a6d7a1d758e17b65de4a01c9e1fff421074
wangjuanjuan <806960722@qq.com>
2019-06-06 15:20:55 +0800

    support wireless keypad and hotel lock

---
+++ TTLockSourceCodeDemo/Models/KeypadModel.h
@@ -0,0 +1,20 @@
+//
+//  KeypadModel.h
+//  TTLockSourceCodeDemo
+//
+//  Created by Jinbo Lu on 2019/5/28.
+//  Copyright © 2019 Sciener. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface KeypadModel : NSObject
+@property (nonatomic, strong) NSString *wirelessKeyboardId;
+@property (nonatomic, strong) NSString *wirelessKeyboardNumber;
+@property (nonatomic, strong) NSString *wirelessKeyboardMac;
+@property (nonatomic, strong) NSString *wirelessKeyboardName;
+@end
+
+
---
+++ TTLockSourceCodeDemo/Models/KeypadModel.m
@@ -0,0 +1,13 @@
+//
+//  KeypadModel.m
+//  TTLockSourceCodeDemo
+//
+//  Created by Jinbo Lu on 2019/5/28.
+//  Copyright © 2019 Sciener. All rights reserved.
+//
+
+#import "KeypadModel.h"
+
+@implementation KeypadModel
+
+@end
