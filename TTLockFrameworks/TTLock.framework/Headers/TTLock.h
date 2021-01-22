81fcefd683e42073fce8d89dd3d4db63bf81642c
wangjuanjuan <806960722@qq.com>
2021-01-22 18:40:48 +0800

    Fix the lift bug

--- TTLockFrameworks/TTLock.framework/Headers/TTLock.h
+++ TTLockFrameworks/TTLock.framework/Headers/TTLock.h
@@ -5,7 +5,7 @@
 //  Created by Jinbo Lu on 2019/4/23.
 //  Copyright © 2019 Sciener. All rights reserved.
 
-//  version:3.1.6
+//  version:3.1.7
 
 #import <Foundation/Foundation.h>
 #import "TTBlocks.h"
