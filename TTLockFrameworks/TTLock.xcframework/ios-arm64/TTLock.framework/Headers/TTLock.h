154a46adb36813839924bbba3c0f5bee0b0be3af
wangjuanjuan9 <806960722@qq.com>
2025-02-28 10:01:43 +0800

    Support G5 gateway and Electric Meter

--- TTLockFrameworks/TTLock.xcframework/ios-arm64/TTLock.framework/Headers/TTLock.h
+++ TTLockFrameworks/TTLock.xcframework/ios-arm64/TTLock.framework/Headers/TTLock.h
@@ -5,7 +5,7 @@
 //  Created by Jinbo Lu on 2019/4/23.
 //  Copyright © 2019 Sciener. All rights reserved.
 
-//  version:3.4.8
+//  version:3.4.6
 
 #import <Foundation/Foundation.h>
 #import <TTLock/TTBlocks.h>
